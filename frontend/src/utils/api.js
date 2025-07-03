19d3b97f9d63f02e80258582371bf49224c2ee03
vishnuvardhan7569 <reddypvishnuvardhan760@gmail.com>
2025-07-03 23:37:40 +0530

    Use environment variable for API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 export const searchBusesByStop = async (stop) => {
-  const response = await axios.get('http://localhost:5000/api/buses/search/stop', {
+  const response = await axios.get(`${API_BASE_URL}/buses/search/stop`, {
     params: { stop }
   });
   return response.data;
 };
 
 export const fetchAllBusesForDropdown = async () => {
-  const response = await axios.get('http://localhost:5000/api/buses/test');
+  const response = await axios.get(`${API_BASE_URL}/buses/test`);
   return response.data.buses;
 };
